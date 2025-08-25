a447f521d1eae8952f71a4db038865bd01b6ae13
HyeonggyuKang <kin0204@naver.com>
2025-08-25 09:34:08 +0900

    Open-Closed(개방 폐쇄 원칙)
     1. 기능 확장시 기존 코드의 수정없이 확장이 가능 해야 함
     2. 기존 코드의 변경을 하지 않도록 payment 인터페이스를 만들어 다향성을 적욜해 처리 하도록 로직 수정'

---
+++ chap17-sollid-lecture-source/src/main/java/com/ohgiraffers/section01/section02/openclosed/problem/Application.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section01.section02.openclosed.problem;
+
+public class Application {
+}
---
+++ chap17-sollid-lecture-source/src/main/java/com/ohgiraffers/section01/section02/openclosed/resolved/NaverPayPayment.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section01.section02.openclosed.resolved;
+
+public class NaverPayPayment {
+}
